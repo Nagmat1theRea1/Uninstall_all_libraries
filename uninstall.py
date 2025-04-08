f4204c5cfb0f5fd34da7439263a2cce524756cf1
Nagmat1theRea1 <106673127+Nagmat1theRea1@users.noreply.github.com>
2025-04-08 13:53:31 +0200

    Created uninstall.py
    With this program, you will be able to delete all python libraries.

---
+++ uninstall.py
@@ -0,0 +1,28 @@
+import subprocess
+
+command = ["pip list", "pip uninstall"]
+
+help_result = subprocess.run(
+    ["powershell", "-Command", command[0]],
+    capture_output=True,
+    text=True
+)
+
+
+delete = ""
+pwsAllinAll = str(help_result)
+pwsSeparte = pwsAllinAll.split("\\n")
+for i in range(len(pwsSeparte)-1):
+    if i < 2:
+        i += 1
+    else:
+        for j in range(len(pwsSeparte[i])):
+            if pwsSeparte[i][j] != " ":
+                delete = delete + pwsSeparte[i][j]
+            else:
+                subprocess.run(
+                    ["powershell", "-Command", command[1]+" "+ delete + " --y"],
+                    text=True
+                    )
+                break
+        delete = ""
